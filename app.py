d8752c95ade7b472e8d3e99d3a8f1f648a3756dc
fusionby2030 <adamkit11@gmail.com>
2021-01-03 11:46:55 +0100

    NN Update + errors

--- app.py
+++ app.py
@@ -1,12 +1,87 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, jsonify
+from NN_sache.image_class import *
+from keras.applications import ResNet50
 
 app = Flask(__name__)
+model = None
+
+
+def load_model():
+    """
+    Load Model Function:
+    Pretrained ImageNet and provided by Keras,
+    To be substituted with self model
+    """
+
+    global model
+    model = ResNet50(weights="imagenet")
+
+
+# Catch type errors when submitting something other than an image
+@app.errorhandler(TypeError)
+def internal_error(error):
+    return render_template('500.html'), 500
+
+
+@app.errorhandler(404)
+def internal_error(error):
+    return render_template('404.html'), 404
 
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
-if __name__=='__main__':
+
+@app.route('/human_error')
+def musik():
+    return render_template('human_error.html')
+
+
+@app.route('/classify')
+def classify():
+    return render_template('classify.html')
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    # initialize the data dictionary that will be returned from the
+    # view
+    data = {"success": False}
+    print(data)
+
+    # ensure an image was properly uploaded to our endpoint
+    if request.method == "POST":
+        if request.files.get("image"):
+
+            # read the image in PIL format
+            image = request.files["image"].read()
+            print(image)
+            image = open_image(image)
+
+            # preprocess the image and prepare it for classification
+            image = prepare_image(image, target=(224, 224))
+
+            # classify the input image and then initialize the list
+            # of predictions to return to the client
+            preds = model.predict(image)
+            results = decode_predictions(preds)
+            data["predictions"] = []
+
+            # loop over the results and add them to the list of
+            # returned predictions
+            for (imagenetID, label, prob) in results[0]:
+                r = {"label": label, "probability": round(float(prob), 2)}
+                data["predictions"].append(r)
+
+            # indicate that the request was a success
+            data["success"] = True
+        else:
+            raise TypeError
+    # return the data dictionary as a JSON response
+    return render_template('predictions.html', data=data["predictions"])
+
+
+if __name__ == '__main__':
+    load_model()
     app.run(debug=True)
